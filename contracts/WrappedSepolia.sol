2fa2c2d5527cd19337ecb6acec24d51c4b159e6c
toryoto <ryoto.taga12@gmail.com>
2025-02-24 18:21:31 +0900

    wrappedSepolia contract

---
+++ contracts/WrappedSepolia.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract WrappedSepolia is ERC20, Ownable {
+    event Deposit(address indexed from, uint256 amount);
+    event Withdrawal(address indexed to, uint256 amount);
+
+    constructor() ERC20("Wrapped Sepolia", "WSEP") Ownable(msg.sender) {
+    }
+
+    function deposit() public payable {
+        require(msg.value > 0, "Must deposit some ETH");
+        _mint(msg.sender, msg.value);
+        emit Deposit(msg.sender, msg.value);
+    }
+
+    function withdraw(uint256 amount) public {
+        require(amount > 0, "Must withdraw some ETH");
+        require(balanceOf(msg.sender) >= amount, "Insufficient balance");
+
+        _burn(msg.sender, amount);
+        (bool success, ) = msg.sender.call{value: amount}("");
+        require(success, "ETH transfer failed");
+        emit Withdrawal(msg.sender, amount);
+    }
+}
